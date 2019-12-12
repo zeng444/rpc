289c832b42a50fcbe4a6c3f6d61cbd030b5a62dc
Robert <zeng444@163.com>
2019-12-12 21:01:44 +0800

    更新一个批量请求

---
+++ src/Server/Dispatcher/Batch.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Janfish\Rpc\Server\Dispatcher;
+
+use Janfish\Rpc\Server\Dispatcher;
+use Janfish\Rpc\Server\Exception;
+
+
+class Batch
+{
+
+    /**
+     * Author:Robert
+     *
+     * @var array
+     */
+    protected $callData = [];
+
+    /**
+     * Author:Robert
+     *
+     * @var
+     */
+    protected $afterInstanced;
+
+    /**
+     * Author:Robert
+     *
+     * @var 
+     */
+    protected $afterFindOut;
+
+    /**
+     * Dispatcher constructor.
+     * @param $data
+     * @throws Exception
+     */
+    public function __construct($data)
+    {
+        if (!isset($data['batch']) || !is_array($data['batch'])) {
+            throw new Exception('参数错误');
+        }
+        $this->callData = $data['batch'];
+    }
+
+
+    /**
+     * Author:Robert
+     *
+     * @throws Exception
+     */
+    public function run()
+    {
+        $result = [];
+        foreach ($this->callData as $data) {
+            $dispatch = new Dispatcher($data);
+            $dispatch->afterFindOut($this->afterFindOut);
+            $dispatch->afterInstanced($this->afterInstanced);
+            $result[] = $dispatch->run();
+        }
+        return $result;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param $method
+     */
+    public function afterInstanced($method): void
+    {
+        $this->afterInstanced = $method;
+    }
+
+
+    /**
+     * Author:Robert
+     *
+     * @param $loader
+     */
+    public function registerAutoLoad($loader): void
+    {
+        static $rpcLoaderInit = false;
+        if (!$rpcLoaderInit) {
+            spl_autoload_register($loader);
+            $rpcLoaderInit = true;
+        }
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param $method
+     */
+    public function afterFindOut($method): void
+    {
+        $this->afterFindOut = $method;
+    }
+
+}
