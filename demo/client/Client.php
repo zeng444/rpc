e6d1c95e6d99e4a21a382412c044ed452f05dbce
Robert <zeng444@163.com>
2019-12-21 15:00:31 +0800

    更新demo

--- demo/client/Client.php
+++ demo/client/Client.php
@@ -5,47 +5,42 @@ use Janfish\Rpc\Client;
 include '../../vendor/autoload.php';
 $services = require_once 'configs/services.php';
 
-try {
 
-    //单次调用
-    Client::init($services);
-    $data = Services\CallCenter\User\Profile::getById();
-    print_r($data.PHP_EOL);
+//单次调用
+Client::init($services);
+$data = Services\CallCenter\User\Profile::getById();
+print_r($data.PHP_EOL);
 
-    //单次调用
-    $clientBatch = new Client\Batch($services);
-    $data = $clientBatch->call([
+//单次调用
+$clientBatch = new Client\Batch($services);
+$data = $clientBatch->call([
+    'class' => 'Services\CallCenter\User\Profile',
+    'method' => 'getById',
+    'args' => ['1'],
+]);
+print_r($data.PHP_EOL);
+//批量调用
+$commands = [
+    "user1" => [
         'class' => 'Services\CallCenter\User\Profile',
         'method' => 'getById',
         'args' => ['1'],
-    ]);
-    print_r($data.PHP_EOL);
-    //批量调用
-    $commands = [
-        "user1" => [
-            'class' => 'Services\CallCenter\User\Profile',
-            'method' => 'getById',
-            'args' => ['1'],
-        ],
-        "user2" => [
-            'class' => 'Services\CallCenter\User\Profile',
-            'method' => 'getById',
-            'args' => [],
-        ],
-        "user3" => [
-            'class' => '\Services\CallCenter\User\Profile',
-            'method' => 'getById',
-            'args' => ['2'],
-        ],
-    ];
-    //显式调用
-    $clientBatch = new Client\Batch($services);
-    $data = $clientBatch->call($commands);
-    print_r($data);
-    //隐式调用
-    $data = Services\Client::call($commands);
-    print_r($data);
+    ],
+    "user2" => [
+        'class' => 'Services\CallCenter\User\Profile',
+        'method' => 'getById',
+    ],
+    "user3" => [
+        'class' => '\Services\CallCenter\User\Profile',
+        'method' => 'getById',
+        'args' => ['2'],
+    ],
+];
+//显式调用
+$clientBatch = new Client\Batch($services);
+$data = $clientBatch->call($commands);
+print_r($data);
+//隐式调用
+$data = Services\Client::call($commands);
+print_r($data);
 
-} catch (\Exception  $e) {
-    echo $e->getMessage();
-}
